dda82962799c4cc063c6d8858bb2f136392e62f0
SM822 <SM822@DESKTOP-L5TQ3PO>
2020-12-07 11:41:01 +0900

    설명문

--- 3rd_project/src/frontcontroller/ReadRead.java
+++ 3rd_project/src/frontcontroller/ReadRead.java
@@ -5,8 +5,9 @@ public class ReadRead {
 	// 0. 3rd_project Synchronize를 제일 먼저 하기!
 	
 	// 1. pull로 불러오기!
-	// 2. commit
-	// 3. push
+	// 2. 자신이 변경한 클래스 or 3rd_project 전체를 add to index
+	// 3. commit
+	// 4. push to branch master
 	
 	
 	
