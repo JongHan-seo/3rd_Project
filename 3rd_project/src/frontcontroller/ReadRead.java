60679f582f51eb0bf3a5a44d1786cfc30766eb3e
SM804 <SM804@DESKTOP-1GUDNCA>
2020-12-07 12:13:48 +0900

    DB 작성중

---
+++ 3rd_project/src/frontcontroller/ReadRead.java
@@ -0,0 +1,20 @@
+package frontcontroller;
+
+public class ReadRead {
+
+	public static void main(String[] args) {
+		
+		// 이거 먼저 읽고 사용할 것!!
+		
+		// 1. 3rd_project - Synchronize를 제일 먼저!
+		// 2. 내려받을 파일이 있으면 pull
+		// 3. 자신의 
+		
+		
+		// pull, push 하기 전에 
+		
+		
+		
+	}
+
+}
