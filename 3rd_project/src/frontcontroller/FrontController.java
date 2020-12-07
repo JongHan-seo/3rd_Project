f95f9fe3d5cf9e255f5203d3e44b3748f89bbc02
SM822 <SM822@DESKTOP-L5TQ3PO>
2020-12-07 10:34:59 +0900

    3차 프로젝트 템플릿

---
+++ 3rd_project/src/frontcontroller/FrontController.java
@@ -0,0 +1,19 @@
+package frontcontroller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/"+nextView+".jsp");
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/main.html");
+		rd.forward(request, response);
+	}
+
+}
